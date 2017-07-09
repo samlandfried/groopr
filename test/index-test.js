fa6ded7506cfd228eedf9a19c988260245db696b
Sam Landfried <samlandfried@gmail.com>
2017-07-09 14:27:46 -0600

    Setup Selenium for testing

---
+++ test/index-test.js
@@ -0,0 +1,31 @@
+const assert = require('chai').assert;
+const webdriver = require('selenium-webdriver');
+const until = webdriver.until;
+const test = require('selenium-webdriver/testing');
+
+test.describe('Poodr', function() {
+  this.timeout(10000);
+
+  const addr = 'http://localhost:3000';
+  let driver;
+
+  test.beforeEach(() => {
+    driver = new webdriver.Builder()
+      .forBrowser('chrome')
+      .build()
+  });
+
+  test.afterEach(() => {
+    driver.quit();
+  });
+
+  test.it('Loads', () => {
+    driver.get(addr);
+    driver.wait(until.elementLocated({ css: '.App' }));
+    driver.findElement({ css: '.App-header h2' })
+      .getText()
+      .then(header => {
+        assert.equal(header, 'Welcome to React');
+      });
+  });
+});
