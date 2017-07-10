4e39c39487735c21f7e7522c9eebc0e91e843360
Sam Landfried <samlandfried@gmail.com>
2017-07-10 17:54:23 -0600

    Setup OAuth with Auth0

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.css';
 import App from './App';
+import { makeMainRoutes } from './routes';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const routes = makeMainRoutes();
+ReactDOM.render(routes, document.getElementById('root'));
 registerServiceWorker();
