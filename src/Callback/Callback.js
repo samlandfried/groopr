833007ea4e37012644ea37eb0abf8f938f4a3c64
Sam Landfried <samlandfried@gmail.com>
2017-07-24 09:56:11 -0600

    Retrieve team credentials from Slack

--- src/Callback/Callback.js
+++ src/Callback/Callback.js
@@ -5,9 +5,10 @@ import { AUTH } from './../tokens';
 export default class Callback extends Component {
   componentDidMount() {
     const code = getCode(this.props.location.search);
-    // https://slack.com/api/oauth.access?client_id=asdf&client_secret=asdf&code=asdf&redirect_uri=asdf&pretty=1
-    console.log(AUTH)
-    debugger
+    const url =  `https://slack.com/api/oauth.access?client_id=${AUTH.clientId}&client_secret=${AUTH.clientSecret}&code=${code}&pretty=1`
+    fetch(url)
+    .then(resp => resp.json())
+    .then(data => { debugger } )
   }
 
   render(props) {
